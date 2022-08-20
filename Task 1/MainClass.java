4c3ccb6f02aeffa65ce7334ec390d4e05e2a2a8a
Shravani94 <shravani.a@techouts.co.in>
2022-08-20 10:39:48 +0530

    oops tasks

---
+++ Task 1/MainClass.java
@@ -0,0 +1,13 @@
+package TaskNo1;
+
+public class MainClass {
+	public static void main(String[] agrs)
+	{
+		MobileNumber mn=new MobileNumber();
+		mn.saveContact("nibba",9876543210l);
+		mn.saveContact("nibbi",9876543211l,6789054321l);
+		
+		
+	}
+
+}
---
+++ Task 1/MobileNumber.java
@@ -0,0 +1,23 @@
+package TaskNo1;
+
+public class MobileNumber {
+	public void saveContact(String Name,long MobileNumber1)
+	{
+		System.out.println("It's about the person who is having one contact number");
+		System.out.println("------------------------------------------------------");
+		System.out.println("ContactName : "+Name);
+		System.out.println("First mobile number : "+MobileNumber1);
+		System.out.println();
+		
+	}
+	public void saveContact(String Name,Long MobileNumber1,long MobileNumber2)
+	{
+		System.out.println("It's about the person who is having one contact number");
+		System.out.println("------------------------------------------------------");
+		System.out.println("ContactName : "+Name);
+		System.out.println("First mobile number : "+MobileNumber1);
+		System.out.println("Second mobile number : "+MobileNumber2);
+		
+		
+	}
+}
